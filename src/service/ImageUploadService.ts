25bc6e27a6e25e70aacec176fdceed046ec556d5
GyuBa <sansam1010@naver.com>
2023-02-08 17:09:58 +0900

    Feat: basic file upload complete

--- src/service/ImageUploadService.ts
+++ src/service/ImageUploadService.ts
@@ -4,10 +4,15 @@ import {DIR_PATH_UPLOADED_IMAGE} from "../constant/constants";
 export async function uploadImage(req: Request, res: Response, next: Function) {
     const uploadFile = req.files.file;
     console.log(uploadFile);
+    console.log(DIR_PATH_UPLOADED_IMAGE)
     if ("mv" in uploadFile) {
-        uploadFile.mv(DIR_PATH_UPLOADED_IMAGE, function (err) {
-            if (err)
-                return res.status(500).send(err);
-        });
+        const path = 'uploads/' + uploadFile.name;
+        uploadFile.mv(
+            path,
+            function (err) {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+            });
     }
 }
