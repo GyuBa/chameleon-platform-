134b0763cfbac70b43ab788968bec5ce841c1369
GyuBa <sansam1010@naver.com>
2023-02-09 04:09:40 +0900

    Feat: Image Controller

---
+++ src/controller/ImageController.ts
@@ -0,0 +1,50 @@
+/* TODO
+ [x] create
+ [x] find by id, property
+ [ ] update
+ */
+
+import {RegionInterface} from "../interface/RegionInterface";
+import {source} from "../DataSource";
+import {Region} from "../entities/Region";
+import {Image} from "../entities/Image";
+import {ImageInterface} from "../interface/ImageInterface";
+
+export async function createImage(imageInput: ImageInterface, region: Region) {
+    const imageRepository = source.getRepository('Image');
+    try {
+        const image = new Image();
+        image.repository = imageInput.repository;
+        image.tags = imageInput.tags;
+        image.region = region;
+        await imageRepository.save(image);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export async function findRegionById(id: number) {
+    const imageRepository = source.getRepository('Image');
+    try {
+        return await imageRepository
+            .createQueryBuilder('image')
+            .select()
+            .where('id=:id', {id: id})
+            .getOne();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export async function findRegionByProperty(tags: string, repository: string) {
+    const regionRepository = source.getRepository('Region');
+    try {
+        return await regionRepository
+            .createQueryBuilder('region')
+            .select()
+            .where('tags = :tags AND repository = :repository', {tags: tags, repository: repository})
+            .getOne();
+    } catch (e) {
+        console.error(e);
+    }
+}
---
+++ src/interface/ImageInterface.ts
@@ -0,0 +1,5 @@
+export interface ImageInterface {
+    readonly id: number;
+    repository: string;
+    tags: string;
+}
