d3dfcbe84d974811b8ceef59524a6811dda89550
GyuBa <sansam1010@naver.com>
2023-02-14 06:57:30 +0900

    Feat: Add response message constant

--- src/constant/Constants.ts
+++ src/constant/Constants.ts
@@ -1,1 +1,13 @@
 export const DIR_PATH_UPLOADED_IMAGE = `${__dirname}/temp/image/`;
+
+export const RESPONSE_MSG = {
+    OK: 'ok',
+
+    SERVER_ERROR: 'server_error',
+
+    NON_FIELD: 'non_filed_error',
+    UNABLE_CREDENTIAL: 'unable_credential_errors',
+    DUPLICATED_EMAIL: 'duplicated_email_error',
+    DUPLICATED_NAME: 'duplicated_name_error',
+    NOT_AUTH: 'not_authenticated_error',
+};
