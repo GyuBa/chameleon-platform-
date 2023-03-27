d97cd2622f44d612e92402268634666f2fc581c6
refracta <58779799+refracta@users.noreply.github.com>
2023-03-27 21:13:55 +0900

    Fix BaseService.ts → PlatformService.ts

--- src/server/impl/handler/DefaultWSHandler.ts
+++ src/server/impl/handler/DefaultWSHandler.ts
@@ -1,9 +1,9 @@
 import {RawData} from 'ws';
 import {DefaultWSocket, DefaultWSServer, WebSocketHandler} from '../../../types/chameleon-platform';
-import {BaseService} from '../../../service/interfaces/BaseService';
+import {PlatformService} from '../../../service/interfaces/PlatformService';
 
 
-export default class DefaultWSHandler extends BaseService implements WebSocketHandler<DefaultWSServer, DefaultWSocket> {
+export default class DefaultWSHandler extends PlatformService implements WebSocketHandler<DefaultWSServer, DefaultWSocket> {
     onReady(server: DefaultWSServer, socket: DefaultWSocket) {
         /* empty */
     }
